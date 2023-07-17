1790fd58a896e7b34db695950426728a835a648a
Lunix <ramezjoseph8@gmail.com>
2023-07-17 16:17:19 +0200

    ErrorBoundary

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,16 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import ErrorBoundary from '@/components/ErrorBoundary'
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    
+    <ErrorBoundary>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+    </ErrorBoundary>
   )
 }
