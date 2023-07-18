417f563054daba76713fbdce2d33ca3df9c64a8c
Lunix <ramezjoseph8@gmail.com>
2023-07-18 17:01:40 +0200

    UI/UX Animation

--- client/components/app/Card.js
+++ client/components/app/Card.js
@@ -3,9 +3,9 @@ import Link from 'next/link'
 export default function Card(props) {
     return (
         <>
-    <Link className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer" href={props.href}>
+    <Link className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer duration-300" href={props.href}>
   <div className="card-body">
-    <h2 className="card-title font-bold">{props.title}</h2>
+    <h2 className="card-title font-extrabold">{props.title}</h2>
     <br />
     <Link href='/' className="font-medium -mb-3 duration-150">{props.website}</Link>
     <div className="card-actions justify-end">
--- client/components/app/Loading.js
+++ client/components/app/Loading.js
@@ -9,7 +9,7 @@ export default function Loading() {
     <br />
     <br />
     <div className='flex flex-wrap gap-8 justify-center w-full'>
-        <div className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer">
+        <div className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer duration-300">
 <div role="status" class="max-w-sm animate-pulse">
 <div className="card-body">
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[100px] mb-2.5 card-title"></div>
@@ -19,7 +19,7 @@ export default function Loading() {
     </div>
 </div>
 </div>
-<div className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer">
+<div className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer duration-300">
 <div role="status" class="max-w-sm animate-pulse">
 <div className="card-body">
     <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[100px] mb-2.5 card-title"></div>
