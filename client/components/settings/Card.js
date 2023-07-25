00f0893b8635e1d1052b4875f19b5ee85e8193c9
Lunix <ramezjoseph8@gmail.com>
2023-07-25 17:52:21 +0200

    settings page

--- client/components/settings/Card.js
+++ client/components/settings/Card.js
@@ -1,12 +1,12 @@
 export default function Card(props) {
     return (
         <>
-        <div className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer duration-300 outline-none">
+        <div className="card sm:w-4/5 lg:w-2/4 bg-gray-900 shadow-xl cursor-pointer duration-300 outline-none">
   <div className="card-body">
-    <div>
-        <input type="text" placeholder="Type here" className="input w-full max-w-xs" value={props.projectName} />
-        <br /><br />
-        <input type="text" placeholder="Type here" className="input w-full max-w-xs" value={props.projectId}/>
+    <div className="flex flex-wrap gap-3 justify-center">
+        
+        <input type="text" className="input w-4/5 bg-gray-950 font-medium" value={props.projectName} />
+        <input type="text" className="input w-4/5 bg-gray-950 font-medium" value={props.projectId}/>
     </div>
     <br />
     <div className="card-actions justify-end">
