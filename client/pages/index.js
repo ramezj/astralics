3da2a9c26a6ca4824fa3f431acd75959195b7fb1
Lunix <ramezjoseph8@gmail.com>
2023-07-31 19:07:44 +0200

    widget paused

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 import Layout from "@/components/layout"
 import Widget from "@/components/widget"
-import { BlitzFeedback } from "blitzfeedbacktest"
+// import { BlitzFeedback } from "blitzfeedbacktest"
 
 export default function Component() {
   const { data: session } = useSession()
@@ -10,7 +10,7 @@ export default function Component() {
       <center>
         <br />
         <Widget projectId={'clkq68fsg0001us3w0522kvfx'} />
-        <BlitzFeedback name="Ramez Joseph" />
+        {/* <BlitzFeedback name="Ramez Joseph" /> */}
       </center>
     </Layout>
   )
