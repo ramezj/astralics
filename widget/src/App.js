4f15eff2f2e58ae68a42b7dbfe4658d662aaf08a
Lunix <ramezjoseph8@gmail.com>
2023-07-31 17:03:24 +0200

    CORS test

--- widget/src/App.js
+++ widget/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import BlitzFeedback from './lib/components/BlitzFeedback';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BlitzFeedback projectId={'clkq68fsg0001us3w0522kvfx'}/>
+    </>
+  )
 }
 
 export default App;
