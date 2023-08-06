bc82bf094553ef4cb5f58604a8c30918576e8a0c
Lunix <ramezjoseph8@gmail.com>
2023-08-06 02:51:37 +0200

    test blitzfeedback widget 0.1.2

--- pckg/src/BlitzFeedback.tsx
+++ pckg/src/BlitzFeedback.tsx
@@ -3,7 +3,11 @@ import * as React from 'react';
 export function BlitzFeedback(props:{projectId:any}) {
     return (
         <>
-            <p>BlitzFeedback Welcomes u from TSDX, {props.projectId}</p>
+            
+<a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+    <p className='font-normal text-gray-700 dark:text-gray-400'>BlitzFeedback Welcomes u from TSDX, {props.projectId}</p>
+</a>
         </>
     )
 }
---
+++ pckg/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
