c41ec999718a0ef8a02cd3cbef24db1b68f511ea
Lunix <ramezjoseph8@gmail.com>
2023-08-06 17:46:31 +0200

    [BlitzPackagev2]

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,6 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 import Layout from "@/components/layout"
 import Widget from "@/components/widget"
-// import { BlitzFeedback } from "blitzpackage"
 import { BlitzFeedback } from "blitzpackagev2"
 
 export default function Component() {
@@ -11,8 +10,7 @@ export default function Component() {
       <center>
         <br />
         <Widget projectId={'clkt435x30003jq09tj4h214a'} />
-        {/* <BlitzFeedback projectId={"Ramez TSDX :D"} /> */}
-        <BlitzFeedback />
+        <BlitzFeedback projectId={'Ramez Joseph'} />
         <e />
       </center>
     </Layout>
