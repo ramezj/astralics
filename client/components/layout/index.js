ffc30e8bdd6b8801078682e31c37f28192dacfab
Lunix <ramezjoseph8@gmail.com>
2023-08-11 14:13:32 +0200

    Purple gradient background [test]

--- client/components/layout/index.js
+++ client/components/layout/index.js
@@ -2,7 +2,7 @@ import Navbar from "../navbar";
 
 export default function Layout(props) {
     return (
-        <div className='h-full min-h-screen bg-gray-950'>
+        <div className='h-full min-h-screen bg-gradient-to-br from-gray-950 to-[#0d041e]'>
         <Navbar/>
         {props.children}
         </div>
