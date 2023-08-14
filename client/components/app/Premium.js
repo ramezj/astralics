e2e0731ecfba87de90ca12188bbf0d19822babba
Lunix <ramezjoseph8@gmail.com>
2023-08-14 13:50:21 +0200

    <a /> fix

--- client/components/app/Premium.js
+++ client/components/app/Premium.js
@@ -8,14 +8,14 @@ export default function Premium(props) {
         {
       props.premium 
       ? <><span className="flex w-48 items-center premiumShadow align-middle bg-green-600 text-white text-md font-bold px-10 py-2 rounded-full dark:bg-green-500 dark:text-white">Premium Plan</span></>
-      : <> <span className="flex w-48 items-center alertShadow align-middle cursor-pointer bg-red-600 text-white text-md font-bold px-10 py-2 rounded-full dark:bg-red-600 dark:text-white">
-        <a onClick={(() => {
-            Paddle.Checkout.open({
-              product:63325,
-              success:'/success'
-            })
-        })} className=''>Upgrade Plan</a>
-        </span> <>
+      : <> <a onClick={(() => {
+        Paddle.Checkout.open({
+          product:63325,
+          success:'/success'
+        })
+    })} className=''><span className="flex w-48 items-center alertShadow align-middle cursor-pointer bg-red-600 text-white text-md font-bold px-10 py-2 rounded-full dark:bg-red-600 dark:text-white">
+        Upgrade Plan
+        </span></a> <>
       </></>
     }
         </>
