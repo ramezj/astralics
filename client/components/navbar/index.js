df2df79fd753f053951abc6971225e0ba4053cac
Lunix <ramezjoseph8@gmail.com>
2023-08-16 02:21:13 +0200

    test fix

--- client/components/navbar/index.js
+++ client/components/navbar/index.js
@@ -34,10 +34,12 @@ export default function Navbar(props) {
     </button> */}
     <div className="dropdown dropdown-end">
   <label tabIndex={0} className="btn bg-transparent border-none hover:bg-gray-950">
+    <div className='flex items-center gap-2'>
             <img className='w-8 rounded-full' src={session.user.image}/>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
             </svg>
+    </div>        
   </label>
   <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow rounded-box bg-[#0d041e] w-52">
     <li><Link href='/settings'>Account Settings</Link></li>
