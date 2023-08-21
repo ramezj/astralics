73d37fddb60495a763473bd272d7a967d560330b
Lunix <ramezjoseph8@gmail.com>
2023-08-21 19:38:50 +0200

    pricing debug test ( no buttons )

--- client/pages/pricing/index.js
+++ client/pages/pricing/index.js
@@ -23,9 +23,18 @@ export default function Pricing(props) {
             </center>
             <br /><br />
             <div className='flex flex-wrap gap-12 justify-center w-full'>
-                <PricingCard price='Free' title='Starter' button='Get Started' background={'whiteBackground'} shadow={''} />
-                <PricingCard price='Free' title='Starter' button='Get Started' background={'whiteBackground'} shadow={''} />
-                <PricingCard price='Free' title='Starter' button='Get Started' background={'whiteBackground'} shadow={''} />
+                <PricingCard price='Free' title='Free' button='Get Started' background={'whiteBackground'} shadow={''}>
+                <Info><Svg /> 1 Project</Info>
+                <Info><Svg /> 1 Project</Info>
+                </PricingCard >
+                <PricingCard price='$5.99' title='Pro' button='Get Started' background={'backgroundColor'} shadow={'widgetShadow'}>
+                <Info><Svg /> 3 Project</Info>
+                <Info><Svg /> 1 Project</Info>
+                </PricingCard >
+                <PricingCard price='$75.00' title='Teams' button='Get Started' background={'whiteBackground'} shadow={''}>
+                <Info><Svg /> Unlimited Projects</Info>
+                <Info><Svg /> 1 Project</Info>
+                </PricingCard >
             </div>  
             {/* <div className='flex flex-wrap gap-12 justify-center w-full'>
                 <PricingCard price='Free' title='Starter' button='Get Started' background={'whiteBackground'} shadow={''}>
