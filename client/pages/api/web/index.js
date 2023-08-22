73b7ef6eedbbbca52552a48bec0ecf4c667e3421
Lunix <ramezjoseph8@gmail.com>
2023-08-22 19:20:48 +0200

    webhooks test

--- client/pages/api/web/index.js
+++ client/pages/api/web/index.js
@@ -1,4 +1,6 @@
 // for paddle webhooks
+import { prisma } from "@/lib/db"
+
 export default async function handler(req, res) {
     if(req.method == 'GET') {
         return res.status(400).json({
@@ -6,5 +8,19 @@ export default async function handler(req, res) {
             response:'Invalid Method'
         })
     }
-    console.log(req.body);
+    if(req.body.alert_name == "subscription_created") {
+        try {
+            const updateUser = await prisma.user.update({
+                where: {
+                    id:req.body.passthrough
+                },
+                data: {
+                    premium: true,
+                    level:1
+                }
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    }
  }
--- client/pages/pricing/index.js
+++ client/pages/pricing/index.js
@@ -24,7 +24,7 @@ export default function Pricing(props) {
             <div className='flex flex-wrap gap-12 justify-center w-full'>
                 <PricingCard price='Free' title='Free' button='Get Started' background={'whiteBackground'} shadow={''}>
                 <Info><Svg /> 1 Project</Info>
-                <Info><Svg /> 50 Monthly Feedbacks</Info>
+                <Info><Svg /> 15 Monthly Feedbacks</Info>
                 <br />
                 {
                 session 
@@ -64,56 +64,7 @@ export default function Pricing(props) {
                 : <><button onClick={signUserIn} className='shadow-xl btn w-full text-white normal-case bg-zinc-950 hover:bg-zinc-950 outline-none border-none font-bold rounded-xl'>Get Started</button></>
                 }
                 </PricingCard >
-            </div>  
-            {/* <div className='flex flex-wrap gap-12 justify-center w-full'>
-                <PricingCard price='Free' title='Starter' button='Get Started' background={'whiteBackground'} shadow={''}>
-                <Info><Svg /> 1 Project</Info>
-                <Info><Svg /> 25 Monthly Feedbacks</Info>
-                <Info><Svg /> 1 Project</Info>
-                <Info><Svg /> 1 Project</Info>
-                <br />
-                {
-                session 
-                ? <><Link href='/app' className='shadow-xl btn w-full text-white normal-case bg-zinc-950 hover:bg-zinc-950 outline-none border-none font-bold rounded-xl'>Go to Dashboard</Link></>
-                : <><button onClick={signUserIn} className='shadow-xl btn w-full text-white normal-case bg-zinc-950 hover:bg-zinc-950 outline-none border-none font-bold rounded-xl'>Get Started</button></>
-                }
-                </PricingCard>
-                <PricingCard price='$5.99' title='Pro' button='Get Started' background={'backgroundColor'} shadow={'widgetShadow'}>
-                <Info><Svg /> 3 Projects</Info>
-                <Info><Svg /> Unlimited Feedbacks</Info>
-                <Info><Svg /> 1 Project</Info>
-                <Info><Svg /> 1 Project</Info>
-                <br />
-                {
-                session 
-                ? <><button onClick={(() => {
-                    Paddle.Checkout.open({
-                      product:63325,
-                      success:'/success'
-                    })
-                })} className='shadow-xl btn w-full text-white normal-case bg-zinc-950 hover:bg-zinc-950 outline-none border-none font-bold rounded-xl'>Subscribe Now</button></>
-                : <><button onClick={signUserIn} className='shadow-xl btn w-full text-white normal-case bg-zinc-950 hover:bg-zinc-950 outline-none border-none font-bold rounded-xl'>Get Started</button></>
-                }
-                </PricingCard>
-                <PricingCard price='$75.00' title='Teams' button='Get Started' background={'whiteBackground'} shadow={''}>
-                <Info><Svg /> Unlimited Projects</Info>
-                <Info><Svg /> Unlimited Feedbacks</Info>
-                <Info><Svg /> 1 Project</Info>
-                <Info><Svg /> 1 Project</Info>
-                <br />
-                {
-                session 
-                ? <><button onClick={(() => {
-                    Paddle.Checkout.open({
-                      product:63601,
-                      success:'/success'
-                    })
-                })} className='shadow-xl btn w-full text-white normal-case bg-zinc-950 hover:bg-zinc-950 outline-none border-none font-bold rounded-xl'>Subscribe Now</button></>
-                : <><button onClick={signUserIn} className='shadow-xl btn w-full text-white normal-case bg-zinc-950 hover:bg-zinc-950 outline-none border-none font-bold rounded-xl'>Get Started</button></>
-                }
-                </PricingCard>
-            </div> */}
-            
+            </div>      
         </Layout>
         </>
     )
