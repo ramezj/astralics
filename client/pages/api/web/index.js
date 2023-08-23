8e7bca522c77c50ada1a2a1e2ac406cca268d8c0
Lunix <ramezjoseph8@gmail.com>
2023-08-23 16:35:03 +0200

    premium / teams test

--- client/pages/api/web/index.js
+++ client/pages/api/web/index.js
@@ -9,25 +9,36 @@ export default async function handler(req, res) {
         })
     }
     console.log(req.body);
-    if(req.body.subscription_plan_id == '63325') {
-        console.log('Someone subscribed to the $3.99 plan');
-    }
-    if(req.body.subscription_plan_id == '63601') {
-        console.log('Someone subscribed to the $75.00 plan');
-    }
     if(req.body.alert_name == "subscription_created") {
-        try {
-            const updateUser = await prisma.user.update({
-                where: {
-                    id:req.body.passthrough
-                },
-                data: {
-                    premium: true,
-                    level:1
-                }
-            })
-        } catch (error) {
-            console.error(error);
+        if(req.body.subscription_plan_id == '63325') {
+            try {
+                const updateUser = await prisma.user.update({
+                    where: {
+                        id:req.body.passthrough
+                    },
+                    data: {
+                        premium: true,
+                        level:1
+                    }
+                })
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        if(req.body.subscription_plan_id == '63601') {
+            try {
+                const updateUser = await prisma.user.update({
+                    where: {
+                        id:req.body.passthrough
+                    },
+                    data: {
+                        premium: true,
+                        level:2
+                    }
+                })
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
  }
