8c221d73e3fe7967a306f2bc7da0f6b7d57b74ab
Lunix <ramezjoseph8@gmail.com>
2023-08-27 02:58:00 +0200

    npm i button

--- client/components/Features/index.js
+++ client/components/Features/index.js
@@ -27,14 +27,11 @@ export default function Features(props) {
           Easily collect & analyze bug reports, feature requests, ideas & feedback from multiple websites, all in one dashboard.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
-            className="rounded-md bg-indigo-600 px-10 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Get started
+          <a href="#" className="rounded-md bg-indigo-600 px-10 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          Get started
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-100">
-            Learn more <span aria-hidden="true">→</span>
+          <a href="#" className="rounded-md bg-indigo-600 px-10 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          npm i blitzfeedback@latest
           </a>
         </div>
       </div>
