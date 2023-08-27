5b39123a061917c72fec9d00893740dd90af9c28
Lunix <ramezjoseph8@gmail.com>
2023-08-27 21:10:57 +0200

    copy to clipboard

--- client/pages/documentation/index.js
+++ client/pages/documentation/index.js
@@ -1,12 +1,27 @@
 import Layout from "@/components/layout"
+import { ClipboardDocumentIcon, ClipboardDocumentCheckIcon, CheckIcon } from "@heroicons/react/20/solid"
+import { useState } from "react"
 
 export default function documentation() {
+    const [ npmCopy, setNpmCopy ] = useState(false);
+    const copyNPM = async () => {
+        navigator.clipboard.writeText('npm i blitzpackage@latest');
+        setNpmCopy(true);
+    }
     return (
         <>
         <Layout>
-                <div className="mockup-code w-3/6">
-                <pre data-prefix="$"><code>npm i blitzpackagev2</code></pre>
-                </div>
+            <center>
+            <div className='w-72 py-2.5 bg-indigo-500 rounded-badge'>
+            <p className="font-medium flex justify-center gap-x-3">npm i blitzpackage@latest
+            {
+                npmCopy 
+                ? <><CheckIcon className='flex cursor-pointer' width={24}  height={24} /></>
+                : <><ClipboardDocumentIcon onClick={copyNPM} className='flex cursor-pointer' width={24}  height={24}/></>
+            } 
+            </p>    
+            </div>    
+            </center> 
         </Layout>
         </>
     )
