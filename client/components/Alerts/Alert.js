3cbf2668700c4701db392e8752104eebfe50bd84
Lunix <ramezjoseph8@gmail.com>
2023-08-31 20:19:13 +0200

    new widget colors, new palette, new shadow css

--- client/components/Alerts/Alert.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function Alert(props) {
-    return (
-        <>
- <span className="bg-purple-100 text-purple-800 text-lg font-medium px-10 py-2 rounded-full dark:bg-purple-900 dark:text-purple-200">Launching BlitzFeedback 1.0 🎉</span>
-        </>
-    )
-}
--- client/pages/index.js
+++ client/pages/index.js
@@ -3,15 +3,12 @@ import Layout from "@/components/layout"
 import Widget from "@/components/widget"
 import { BlitzFeedback } from "blitzpackagev2"
 import Features from "@/components/Features"
-import Alert from "@/components/Alerts/Alert"
 
 export default function Component() {
   const { data: session } = useSession()
   return (
     <Layout>
-      {/* <Features /> */}
       <center>
-        {/* <Alert /> */}
           <h1 className='sm:text-5xl 2xl:mt-8 text-4xl w-4/5 font-bold tracking-tight leading-none text-white md:text-6xl lg:text-5xl lg:w-3/6'>
             Feedback is power, collect it with our platform ✨
             </h1>
