11493a98732224e2f18d57847b5b44cc4dc1377c
Lunix <ramezjoseph8@gmail.com>
2023-08-31 17:19:54 +0200

    loading debug

--- client/components/app/Loading.js
+++ client/components/app/Loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
             <center>
                 <br />
                 <br />
-    <div class="h-3 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+    <div class="h-3 bg-gray-700 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
     </center>
     <br />
     <br />
@@ -12,9 +12,9 @@ export default function Loading() {
         <div className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer duration-300">
 <div role="status" class="max-w-sm animate-pulse">
 <div className="card-body">
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[100px] mb-2.5 card-title"></div>
+    <div class="h-2 bg-gray-700 rounded-full dark:bg-gray-700 max-w-[100px] mb-2.5 card-title"></div>
     <br />
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[220px] mb-2.5"></div>
+    <div class="h-2 bg-gray-700 rounded-full dark:bg-gray-700 max-w-[220px] mb-2.5"></div>
     <span class="sr-only">Loading...</span>
     </div>
 </div>
@@ -22,9 +22,9 @@ export default function Loading() {
 <div className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer duration-300">
 <div role="status" class="max-w-sm animate-pulse">
 <div className="card-body">
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[100px] mb-2.5 card-title"></div>
+    <div class="h-2 bg-gray-700 rounded-full dark:bg-gray-700 max-w-[100px] mb-2.5 card-title"></div>
     <br />
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[220px] mb-2.5"></div>
+    <div class="h-2 bg-gray-700 rounded-full dark:bg-gray-700 max-w-[220px] mb-2.5"></div>
     <span class="sr-only">Loading...</span>
     </div>
 </div>
@@ -32,9 +32,9 @@ export default function Loading() {
 <div className="card w-96 bg-gray-900 shadow-xl hover:bg-gray-800 cursor-pointer duration-300">
 <div role="status" class="max-w-sm animate-pulse">
 <div className="card-body">
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[100px] mb-2.5 card-title"></div>
+    <div class="h-2 bg-gray-700 rounded-full dark:bg-gray-700 max-w-[100px] mb-2.5 card-title"></div>
     <br />
-    <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[220px] mb-2.5"></div>
+    <div class="h-2 bg-gray-700 rounded-full dark:bg-gray-700 max-w-[220px] mb-2.5"></div>
     <span class="sr-only">Loading...</span>
     </div>
 </div>
