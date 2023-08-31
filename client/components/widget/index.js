e10470e840ad6d7f2715b7dea489f6acc33f478d
Lunix <ramezjoseph8@gmail.com>
2023-08-31 20:03:22 +0200

    font changed from extrabold to bold

--- client/components/widget/index.js
+++ client/components/widget/index.js
@@ -35,7 +35,7 @@ export default function Widget(props) {
         <>
         <div className="card w-[22.5rem] backgroundColor widgetShadow duration-300">
   <div className="card-body">
-    <h2 className="card-title text-2xl text-white font-black justify-center -mt-2">Send Feedback</h2>
+    <h2 className="card-title text-2xl text-white font-bold justify-center -mt-2">Send Feedback</h2>
     {/* <div className='flex flex-wrap gap-6 justify-center w-full'>
     <span className='shadow-lg bg-white bg-opacity-75 cursor-pointer w-14 h-12 rounded-lg justify-center items-center flex hover:bg-opacity-50 duration-200'>
     <p className='text-2xl flex justify-center'>💡</p>
