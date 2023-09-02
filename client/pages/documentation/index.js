36c9ff7533b4cda3ea78808accb8e9e995040d4a
Lunix <ramezjoseph8@gmail.com>
2023-09-02 19:31:26 +0200

    new documentation layout [ testing ]

--- client/pages/documentation/index.js
+++ client/pages/documentation/index.js
@@ -1,6 +1,7 @@
 import Layout from "@/components/layout"
 import { ClipboardDocumentIcon, ClipboardDocumentCheckIcon, CheckIcon, ClipboardDocumentListIcon } from "@heroicons/react/20/solid"
 import { useState } from "react"
+import DocumentationCard from "@/components/documentation/Card";
 
 export default function documentation() {
     const [ npmCopy, setNpmCopy ] = useState(false);
@@ -13,9 +14,18 @@ export default function documentation() {
         <>
         <Layout>
             <center>
-                <h1 className='font-bold text-3xl'>Documentation</h1>
+                <h1 className='font-bold text-3xl'>API Documentation</h1>
                 <br />
-            <div className='w-72 py-2.5 backgroundColor widgetShadow rounded-lg'>
+                <div className='flex flex-wrap gap-8 justify-center w-4/5'>
+                <DocumentationCard name='Installation 🔨'/>
+                <DocumentationCard name='API key 🔑'/>
+                </div>
+                <br />
+                <div className='flex flex-wrap gap-8 justify-center w-4/5'>
+                <DocumentationCard name='Configuration ⚙️' />
+                <DocumentationCard name='React JS' />
+                </div>
+            {/* <div className='w-72 py-2.5 backgroundColor widgetShadow rounded-lg'>
             <p className="font-medium flex justify-center gap-x-3 drop-shadow-md">npm i blitzpackage@latest
             <span className='w-8 h-6 bg-gray-700 rounded-md flex justify-center items-center'>
             {
@@ -29,7 +39,7 @@ export default function documentation() {
             <br />
             <div className='w-72 py-2.5 bg-indigo-500 rounded-badge'>
             {importText}    
-            </div>    
+            </div>     */}
             </center> 
         </Layout>
         </>
