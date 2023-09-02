bfcba875d3b49b5a32e5a4acfb1caa70fb24405a
Lunix <ramezjoseph8@gmail.com>
2023-09-03 01:21:21 +0200

    card width changed

--- client/components/documentation/Card.js
+++ client/components/documentation/Card.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function DocumentationCard(props) {
     return (
         <div className='gradientBorder p-2 rounded-lg duration-300'>
-        <div className="card w-96 gradient rounded-lg shadow-xl cursor-pointer bg-[#05050a]">
+        <div className="card w-72 gradient rounded-lg shadow-xl cursor-pointer bg-[#05050a]">
         <div className="card-body">
         <h2 className="card-title font-extrabold justify-center align-middle">{props.name}</h2>
         </div>
