3d1297ce152443d7771f7ea670ebc2e49852c1cf
Lunix <ramezjoseph8@gmail.com>
2023-09-03 15:13:11 +0200

    Reply SVG button in /comp/proj/feedback

--- client/components/project/Feedback.js
+++ client/components/project/Feedback.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { EnvelopeOpenIcon } from '@heroicons/react/24/outline';
 export default function Feedback(props) {
     const [ color, setColor ] = useState();
     useEffect(() => {
@@ -16,6 +18,11 @@ export default function Feedback(props) {
   <div className="card-body">
     <h2 className="card-title font-extrabold justify-center">{props.email}</h2>
     <h2 className='float-left flex -mb-2 font-bold justify-center mt-4'>{props.body}</h2>
+    <br />
+    <button href='/app' className='px-8 py-2 bg-black bg-opacity-70 text-white rounded-lg font-bold flex justify-center items-center gap-2'>
+        Reply 
+    <EnvelopeOpenIcon width='18' className='flex content-center'/>
+    </button>
   </div>
 </div>
         </>
