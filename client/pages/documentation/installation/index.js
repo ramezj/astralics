fe54a17b04797c276c2b7bdeef3ae2a59d1cbcfa
Lunix <ramezjoseph8@gmail.com>
2023-09-03 13:22:38 +0200

    npm, pnpm, yarn

--- client/pages/documentation/installation/index.js
+++ client/pages/documentation/installation/index.js
@@ -4,7 +4,15 @@ export default function page() {
     return (
         <>
         <Layout>
-            <p>Installation</p>
+            <center>
+                <br />
+                <h1 className='text-3xl font-bold'>Installation</h1>
+                <br />
+                <br />
+                <h1 className='text-xl font-bold'>npm</h1>
+                <h1 className='text-xl font-bold'>pnpm</h1>
+                <h1 className='text-xl font-bold'>yarn</h1>
+            </center>
         </Layout>
         </>
     )
