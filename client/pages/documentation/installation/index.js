22e83e2007d218b10f501f871bc6363b7ceadd06
daddylunix <ramezjoseph8@gmail.com>
2023-09-07 16:42:56 +0200

    /documentation/installation test

--- client/pages/documentation/installation/index.js
+++ client/pages/documentation/installation/index.js
@@ -11,19 +11,19 @@ export default function page() {
                 <br />
                 <h1 className='text-xl font-bold'>npm</h1>
                 <br />
-                <div className='w-96 py-2 backgroundColor rounded-lg shadow-md'>
+                <div className='w-96 py-2 bg-black bg-opacity-60 rounded-lg shadow-md'>
                     <p className='font-bold'>npm i blitzpackagev2@latest</p>
                 </div>
                 <br />
                 <h1 className='text-xl font-bold'>pnpm</h1>
                 <br />
-                <div className='w-96 py-2 backgroundColor rounded-lg shadow-md'>
+                <div className='w-96 py-2 bg-black bg-opacity-60 rounded-lg shadow-md'>
                     <p className='font-bold'>pnpm i blitzpackagev2@latest</p>
                 </div>
                 <br />
                 <h1 className='text-xl font-bold'>yarn</h1>
                 <br />
-                <div className='w-96 py-2 backgroundColor rounded-lg shadow-md'>
+                <div className='w-96 py-2 bg-black bg-opacity-60 rounded-lg shadow-md'>
                     <p className='font-bold'>yarn add blitzpackagev2@latest</p>
                 </div>
             </center>
