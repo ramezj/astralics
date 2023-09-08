0bbd8eb92c693da84ff8473136d22eb599da07a6
daddylunix <ramezjoseph8@gmail.com>
2023-09-08 03:45:49 +0300

    bg opacity changed

--- client/components/navbar/Feedback.js
+++ client/components/navbar/Feedback.js
@@ -31,7 +31,7 @@ export default function Feedback() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-50" />
+            <div className="fixed inset-0 bg-black bg-opacity-60" />
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
