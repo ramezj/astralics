23b8393f7cf325f06e1fccfce0b449ae800a48bd
Lunix <ramezjoseph8@gmail.com>
2023-09-10 13:04:07 +0200

    outline none for create project button

--- client/components/app/Create.js
+++ client/components/app/Create.js
@@ -49,37 +49,8 @@ export default function Create() {
     return (
         <>
         <button 
-        className='btn bg-black bg-opacity-60 hover:bg-opacity-90 text-white border-none normal-case font-medium px-12' 
+        className='btn bg-black bg-opacity-60 hover:bg-opacity-90 text-white border-none normal-case font-medium px-12 outline-none' 
         onClick={openModal}><FolderPlusIcon width={21}/>Create Project</button>
-<dialog id="my_modal_1" className="modal bg-transparent shadow-2xl">
-  <form method="dialog" className="modal-box bg-gray-950">
-    <center>
-    <h3 className="font-bold text-lg">Project Information</h3>
-    </center>
-    <br /><br />
-    <center>
-    <input autoFocus={false} type="text" placeholder="Project's Name" onChange={(e) => {setName(e.target.value)}} className="input w-full max-w-xs bg-gray-900 font-bold border-none focus:outline-none" />
-    <br /><br />
-    <input autoFocus={false} type="text" placeholder="Project's URL" onChange={(e) => {setUrl(e.target.value)}} className="input w-full max-w-xs bg-gray-900 font-bold border-none focus:outline-none" />
-    <br /><br />
-    <button className="btn widgetShadow backgroundColor font-extrabold text-md w-3/6 text-white border-none hover:bg-indigo-800 normal-case" onClick={createProject}>
-      { 
-      loading 
-      ? <><><span className="loading loading-spinner loading-xs"></span></></>
-      : <>{text}</>
-      }
-      </button>
-      </center>
-    <br /><br />
-    {JSON.stringify(response)}
-    <p className='text-red-500 font-medium'>
-    {error}
-    </p>
-    <div className="modal-action">
-      <button className="btn bg-gray-900 border-none hover:bg-gray-900 normal-case -mt-7 text-white">Close</button>
-    </div>
-  </form>
-</dialog>
 <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
           <Transition.Child
