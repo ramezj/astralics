9107e01e80fcfae2897a384724fce39ea0234d18
daddylunix <ramezjoseph8@gmail.com>
2023-09-11 23:39:02 +0300

    settings card update

--- client/components/settings/src/MainCard.js
+++ client/components/settings/src/MainCard.js
@@ -5,10 +5,10 @@ export default function MainCard(props) {
         <>
         <div className="card w-96 bg-black bg-opacity-60 shadow-xl cursor-pointer duration-300 outline-none">
         <div className="card-body">
-        <img class="w-10 h-10 rounded-full justify-center" src={props.profileAvatar} alt="Rounded avatar" />
-        <br />
+        <center>
+        <img class="w-12 h-12 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar" />
+        </center>
         <h1 className="card-title text-2xl font-bold text-white justify-center">Account Information</h1>
-        <br />
         <label className="float-left flex ml-1">Name</label>
         <input type="text" disabled className="input w-full bg-gray-900 font-medium text-center" value={props.profileName}/>
         <label className="float-left flex ml-1">Email</label>
