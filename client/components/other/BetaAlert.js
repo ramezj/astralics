f68e99cbfabc18aa2152ce6e50806d7c9abc581d
Lunix <ramezjoseph8@gmail.com>
2023-09-13 04:23:07 +0200

    TurboFeedback -> Turbo

--- client/components/other/BetaAlert.js
+++ client/components/other/BetaAlert.js
@@ -1,8 +1,8 @@
 export default function BetaAlert(props) {
     return (
         <>
-        <div className='2xl:w-[24rem] lg:w-96 w-[21rem] rounded-2xl bg-black bg-opacity-60 text-white py-2 2xl:mt-4 2xl:-mb-6 '>
-        <h1 className="font-bold">TurboFeedback v1 just Launched 🎉</h1>
+        <div className='2xl:w-[20rem] lg:w-[20rem] w-[21rem] rounded-2xl bg-black bg-opacity-60 text-white py-2 2xl:mt-4 2xl:-mb-6 '>
+        <h1 className="font-bold">Turbo v1 just Launched 🎉</h1>
         </div>
         </>
     )
