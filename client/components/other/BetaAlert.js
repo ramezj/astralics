07ba034a6c4198eca24eb7ff4222fdd0dff81f1c
Lunix <ramezjoseph8@gmail.com>
2023-09-13 02:24:02 +0200

    alert width responsive

--- client/components/other/BetaAlert.js
+++ client/components/other/BetaAlert.js
@@ -1,8 +1,8 @@
 export default function BetaAlert(props) {
     return (
         <>
-        <div className='w-96 rounded-xl bg-black bg-opacity-60 text-white py-2 2xl:mt-4 2xl:-mb-5 '>
-        <h1 className="font-bold">We're Beta Launching!</h1>
+        <div className='2xl:w-[22rem] lg:w-80 w-[17rem] rounded-2xl bg-black bg-opacity-60 text-white py-2 2xl:mt-4 2xl:-mb-6 '>
+        <h1 className="font-bold">We're Beta Launching 🎉</h1>
         </div>
         </>
     )
