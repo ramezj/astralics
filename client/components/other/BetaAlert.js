258331cd156faa56bb5c600235db764af74271a8
Lunix <ramezjoseph8@gmail.com>
2023-09-15 15:31:45 +0200

    testing another background-color

--- client/components/other/BetaAlert.js
+++ client/components/other/BetaAlert.js
@@ -1,7 +1,7 @@
 export default function BetaAlert(props) {
     return (
         <>
-        <div className='2xl:w-[20rem] lg:w-[20rem] w-[21rem] rounded-2xl bg-black bg-opacity-60 text-white py-2 2xl:mt-4 2xl:-mb-6 alertShadow'>
+        <div className='2xl:w-[20rem] lg:w-[20rem] w-[21rem] rounded-2xl bg-black bg-opacity-60 text-white py-2 2xl:mt-4 2xl:-mb-6'>
         <h1 className="font-bold">Turbo v1 just Launched 🎉</h1>
         </div>
         </>
