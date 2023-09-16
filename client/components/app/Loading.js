43fb3a32e90f9878f37f1887aeff779932d42f79
Lunix <ramezjoseph8@gmail.com>
2023-09-16 16:23:59 +0200

    ...

--- client/components/app/Loading.js
+++ client/components/app/Loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
     <br />
     <br />
     <div className='flex flex-wrap gap-8 justify-center w-full'>
-        <div className="card w-96 bg-black bg-opacity-70 shadow-xl hover:bg-gray-800 cursor-pointer duration-300">
+    <div className="card w-96 bg-black bg-opacity-70 shadow-xl hover:bg-gray-800 cursor-pointer duration-300">
 <div role="status" class="max-w-sm animate-pulse">
 <div className="card-body">
     <div class="h-2 bg-gray-700 rounded-full dark:bg-gray-700 max-w-[100px] mb-2.5 card-title"></div>
