571438209051bd1b8a27b9bb679860ca1d23bc5b
Lunix <ramezjoseph8@gmail.com>
2023-09-18 20:21:02 +0200

    BetaAlert turbo -> Lunar

--- client/components/other/BetaAlert.js
+++ client/components/other/BetaAlert.js
@@ -2,7 +2,7 @@ export default function BetaAlert(props) {
     return (
         <>
         <div className='mt-6 2xl:w-[20rem] lg:w-[20rem] w-[21rem] rounded-2xl bg-black bg-opacity-60 text-white py-2 2xl:mt-4 2xl:-mb-6'>
-        <h1 className="font-bold">Turbo v1 just Launched 🎉</h1>
+        <h1 className="font-bold">Lunar 1.0 just Launched 🎉</h1>
         </div>
         </>
     )
