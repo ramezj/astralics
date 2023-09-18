52080fd850468c7dbea399539999e6f7e03d8602
Lunix <ramezjoseph8@gmail.com>
2023-09-18 03:20:07 +0200

    card update

--- client/components/app/Card.js
+++ client/components/app/Card.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline'
+import { AdjustmentsHorizontalIcon, CodeBracketIcon } from '@heroicons/react/24/outline'
 
 export default function Card(props) {
     return (
@@ -9,10 +9,15 @@ export default function Card(props) {
     <h2 className="card-title font-extrabold drop-shadow-lg">{props.title}</h2>
     <br />
     <h1 className="font-medium -mb-3 duration-150 drop-shadow-lg">{props.website}</h1>
-    <div className="card-actions justify-end">
+    <div className="card-actions justify-end flex gap-6">
+    <span className='-mt-6 -mr-2 w-10 h-8 rounded-lg justify-center items-center float-right flex backgroundColor'>
+    <Link className='drop-shadow-lg float-right shadow-lg flex justify-center' href='/test' >
+    <CodeBracketIcon className='text-white' width={24} height={24} strokeWidth={'2'}/>
+    </Link>
+    </span>
     <span className='-mt-6 -mr-2 w-10 h-8 rounded-lg justify-center items-center float-right flex backgroundColor'>
     <Link href={`${props.settings}`} className='drop-shadow-lg float-right shadow-lg flex justify-center'>
-    <AdjustmentsHorizontalIcon className='text-white' width={24} height={24}/>
+    <AdjustmentsHorizontalIcon className='text-white' width={24} height={24} strokeWidth={'2'}/>
     </Link>
     </span>
     </div>
