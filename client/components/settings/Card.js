8b1a1bdffd852176e9229d73e15520c30c51d7af
Lunix <ramezjoseph8@gmail.com>
2023-09-19 18:57:06 +0200

    bugfix

--- client/components/settings/Card.js
+++ client/components/settings/Card.js
@@ -4,13 +4,13 @@ import SaveChanges from "./SaveChanges"
 export default function Card(props) {
     return (
         <>
-        <div className="card w-96 bg-black bg-opacity-60 shadow-xl duration-300 outline-none">
+        <div className="card w-96 bg-black bg-opacity-80 shadow-xl duration-300 outline-none">
         <div className="card-body">
         <div className="flex flex-wrap gap-3 justify-center">
         <h1 className="card-title text-2xl font-bold text-white justify-center mb-4">Project Information</h1>
-        <input type="text" disabled className="input w-full bg-gray-900 font-medium text-center" value={props.projectName} />
-        <input type="text" disabled className="input w-full bg-gray-900 font-medium text-center" value={props.projectWebsite} />
-        <input type="text" disabled className="input w-full bg-gray-900 font-medium text-center" value={props.projectId}/>
+        <input type="text" disabled className="input w-full bg-gray-950 font-medium text-center" value={props.projectName} />
+        <input type="text" disabled className="input w-full bg-gray-950 font-medium text-center" value={props.projectWebsite} />
+        <input type="text" disabled className="input w-full bg-gray-950 font-medium text-center" value={props.projectId}/>
         <br />
         <div className='flex gap-4 justify-center content-center'>
         <Delete projectId={props.projectId}/>
