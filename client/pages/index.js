fe3c46620abe3ce1eba47ad2d6120ad64e702c52
Lunix <ramezjoseph8@gmail.com>
2023-09-22 22:31:03 +0200

    landing page framer motion transitions

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,6 +5,7 @@ import { BlitzFeedback } from "blitzpackagev2"
 import Features from "@/components/Features"
 import Link from "next/link"
 import BetaAlert from "@/components/other/BetaAlert"
+import { motion } from 'framer-motion'
 
 export default function Component() {
   const { data: session } = useSession()
@@ -13,17 +14,33 @@ export default function Component() {
       <title>Lunar</title>
       <center>
         <div className="">
-          <BetaAlert />
-            <h1 className='
+          <motion.div
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{duration: 1}}
+          >
+              <BetaAlert />
+          </motion.div>
+            <motion.h1 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration: 1,  delay:0.2}}
+            className='
             sm:text-5xl 2xl:text-7xl 2xl:mt-14 text-4xl w-4/5 font-bold tracking-tight leading-none text-transparent bg-clip-text bg-gradient-to-br from-gray-200 via-white to-gray-200 md:text-5xl lg:text-5xl lg:mt-8 lg:w-3/6 sm:mt-8 mt-8'>
             The easiest, fastest way to collect user feedback.
-            </h1>
+            </motion.h1>
             <br />
             {/* <p className='text-gray-100 lg:text-xl lg:w-2/6 md:w-3/6 w-4/5'>An easily embeddable, light-weight feedback widget package that is used to collect user feedback from anywhere.</p> */}
         </div>  
           <br />
           <div className="w-full content-center items-center justify-items-center">
-          <Widget projectId={'clmilxamo0003rt6hwvtibm1f'} title={'Feedback 🪐'} />
+            <motion.div
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration: 1, delay:0.3}}
+            >
+                <Widget projectId={'clmilxamo0003rt6hwvtibm1f'} title={'Feedback 🪐'} />
+            </motion.div>
           </div>
             </center>
     </Layout>
