897169a38365385461b522284d3af24adfb3bce6
daddylunix <ramezjoseph8@gmail.com>
2023-09-27 11:50:47 +0300

    widget change

--- client/components/widget/index.js
+++ client/components/widget/index.js
@@ -46,7 +46,7 @@ export default function Widget(props) {
     <input required value={email} onChange={((e) => {setEmail(e.target.value)})} type="email" placeholder="hello@trylunar.co" className="rounded-xl bg-opacity-50 shadow-xl input w-full mt-2 focus:outline-none bg-neutral-800 text-white font-bold text-sm -mt-1"/>
     <textarea rows='3' required value={feedback} onChange={((e) => {setFeedback(e.target.value)})} className="resize-none rounded-xl bg-opacity-50 shadow-xl textarea w-full mt-3 focus:outline-none bg-neutral-800 text-white font-bold" placeholder="leave your feedback here"></textarea>
     <br /><br />
-    <button type='submit' className='bg-blue-500 hover:bg-blue-600 duration-300 items-center justify-center -mt-3 w-full py-2 rounded-xl text-base text-white normal-caseoutline-none border-none font-bold -mb-3'>
+    <button type='submit' className='bg-blue-500 hover:bg-blue-600 duration-300 items-center justify-center -mt-3 w-full py-2 rounded-xl text-base text-white normal-caseoutline-none border-none font-bold -mb-2'>
                 {
                     loading 
                     ? <><span className="loading loading-spinner loading-xs align-middle"></span></>
