248848958dca64c40adb7a9e8a9c35471be7e580
daddylunix <ramezjoseph8@gmail.com>
2023-09-30 22:50:09 +0300

    widget v2

--- client/components/mvp/index.js
+++ client/components/mvp/index.js
@@ -1,20 +1,39 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { ArrowLeftIcon } from '@heroicons/react/24/outline';
+
 export default function Mvp(props) {
+    const [ step, setStep ] = useState(1);
     return (
         <>
         <div className="card w-[22rem] bg-black">
-            <div className="card-body gap-y-4">
-                <center>
-                    <h1 className="font-bold text-white text-2xl -mt-2 mb-1">How can we help?</h1>
+            <div className="card-body gap-y-5">
+                { step === 1 &&
+                 <>
+                 <center>
+                    <h1 className="font-medium text-white text-2xl -mt-2 mb-1">How can we help?</h1>
                 </center>
-                <span className="rounded-lg bg-blue-600 hover:bg-blue-800 duration-200 py-4 cursor-pointer">
+                <span onClick={(() => {setStep(step+1)})} className="rounded-lg bg-indigo-600 hover:bg-indigo-800 duration-200 py-3.5 cursor-pointer">
                     <h1 className="font-bold">Feature Request</h1>
                 </span>
-                <span className="rounded-lg bg-blue-600 hover:bg-blue-800 duration-200 py-4 cursor-pointer">
+                <span className="rounded-lg bg-indigo-600 hover:bg-indigo-800 duration-200 py-3.5 cursor-pointer">
                     <h1 className="font-bold">Bug Report</h1>
                 </span>
-                <span className="rounded-lg bg-blue-600 hover:bg-blue-800 duration-200 py-4 cursor-pointer">
-                    <h1 className="font-bold">Bug Report</h1>
-                </span>
+                 </>
+                }
+                {
+                step === 2 &&
+                <>
+                <center>
+                <div className='flex gap-x-10'>
+                    <ArrowLeftIcon onClick={(() => {setStep(step-1)})} className='text-gray-200 float-left flex cursor-pointer hover:text-white' width={17} height={17} strokeWidth={'2'}/>
+                <h1 className="font-medium text-white text-2xl -mt-2 mb-1">Feature Request</h1>
+                </div>
+                <textarea rows={3} className='bg-gray-900 rounded-lg w-full mt-4 textarea focus:outline-none' placeholder='I think you shold implement... ' ></textarea>
+                </center>
+                <button className='py-1 bg-indigo-600 text-white rounded-lg font-medium'>Submit</button>
+                </>
+                }
             </div>
         </div>
         </>
