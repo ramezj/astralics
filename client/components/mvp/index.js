37245fa8755b0e1d6c2a06437e4421edbf526179
daddylunix <ramezjoseph8@gmail.com>
2023-10-02 00:57:17 +0300

    some widget updates

--- client/components/mvp/index.js
+++ client/components/mvp/index.js
@@ -6,34 +6,36 @@ export default function Mvp(props) {
     const [ step, setStep ] = useState(1);
     const [ title, setTitle ] = useState('');
     const [ placeholder, setPlaceholder ] = useState('');
+    const [ data, setData ] = useState("");
+    const [ disabled, setDisabled ] = useState(true);
     const featureRequestClick = (e) => {
         e.preventDefault();
-        setTitle('Feature Request');
+        setTitle('💡 Feature Request');
         setPlaceholder('i think you should implement..')
         setStep(step+1);
     }
     const bugReportClick = (e) => {
         e.preventDefault();
-        setTitle('Bug Report');
+        setTitle('🐛 Bug Report');
         setPlaceholder('this is not working on my device..')
         setStep(step+1);
     }
     return (
         <>
-        <div className="card w-[22rem] bg-black">
-            <div className="card-body gap-y-5">
+        <div className="card w-[22rem] h-[13.7rem] bg-black">
+            <div className="card-body gap-y-4">
                 { step === 1 &&
                  <>
                  <center>
-                    <h1 className="font-bold text-white text-xl -mt-2 mb-1">How can we help?</h1>
+                    <h1 className="font-bold text-white text-xl -mt-2 mb-1">How can we improve?</h1>
                 </center>
-                <span onClick={featureRequestClick} className="rounded-lg bg-white hover:bg-gray-300 duration-200 py-3.5 cursor-pointer">
+                <span onClick={featureRequestClick} className="rounded-lg bg-white hover:bg-gray-300 duration-300 py-3.5 cursor-pointer">
                     <h1 className="font-bold text-black">Feature Request</h1>
                 </span>
-                <span onClick={bugReportClick} className="rounded-lg bg-white hover:bg-gray-300 duration-200 py-3.5 cursor-pointer">
+                <span onClick={bugReportClick} className="rounded-lg bg-white hover:bg-gray-300 duration-300 py-3.5 cursor-pointer">
                     <h1 className="font-bold text-black">Bug Report</h1>
                 </span>
-                <a href='https://blitz-feedback.vercel.app' className='text-gray-300 text-xs -mb-5 -mt-2 hover:text-white duration-200 outline-none'>powered by lunar</a>
+                {/* <a href='https://blitz-feedback.vercel.app' className='text-gray-300 text-xs  hover:text-white duration-200'>powered by lunar</a> */}
                  </>
                 }
                 {
@@ -41,17 +43,41 @@ export default function Mvp(props) {
                 <>
                 <center>
                     <div>
-                    <ArrowLeftIcon onClick={(() => {setStep(step-1)})} className='text-gray-200 float-left flex cursor-pointer hover:text-white' width={17} height={17} strokeWidth={'2'}/>
+                    <ArrowLeftIcon onClick={(() => {setStep(step-1)})} className='text-gray-400 duration-200 float-left flex cursor-pointer hover:text-white' width={17} height={17} strokeWidth={'3'}/>
                     </div>
                     <div className='justify-center items-center justify-items-center '>
                         <center>
                         <h1 className="font-bold text-white text-xl -mt-2 mb-1 mr-[1.1rem]">{title}</h1>
                         </center>
                     </div>
-                <textarea rows={3} className='resize-none bg-white text-black placeholder-gray-400 font-bold rounded-lg w-full mt-4 textarea focus:outline-none' placeholder={placeholder}></textarea>
+                <textarea value={data} onChange={((e) => {setData(e.target.value)})} rows={2} className='resize-none bg-white text-black placeholder-gray-400 font-bold rounded-lg w-full mt-4 textarea focus:outline-none' placeholder={placeholder}></textarea>
                 </center>
-                <button className='py-1.5 -mt-2  bg-white hover:bg-gray-200 text-black rounded-lg font-bold'>Submit</button>
-                <a href='https://blitz-feedback.vercel.app' className='text-gray-300 text-xs -mb-5 -mt-2 hover:text-white duration-200'>powered by lunar</a>
+                {/* {
+                    disabled === true && 
+                    <>
+                    <button 
+                className='py-1.5 -mt-2 duration-300 bg-gray-400 cursor-default text-gray-600 rounded-lg font-bold'>
+                Submit
+                </button>
+                    </>
+                } */}
+                {/* {
+                    disabled === false && 
+                    <> */}
+                    <button 
+                    disabled={!data}
+                onClick={(() => {setStep(step+1)})} 
+                className='py-1.5 -mt-2 duration-300 bg-white hover:bg-gray-200 text-black rounded-lg font-bold'>
+                Submit
+                </button>
+                    </>
+                }
+                {/* <a href='https://blitz-feedback.vercel.app' className='text-gray-300 text-xs  hover:text-white duration-200'>powered by lunar</a> */}
+                {/* </>
+                } */}
+                {
+                step === 3 && 
+                <>
                 </>
                 }
             </div>
