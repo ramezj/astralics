c32271f27b776a000e8bc058d732dcf3c3718c2f
daddylunix <ramezjoseph8@gmail.com>
2023-10-07 12:02:19 +0300

    removed blur on bg

--- client/components/navbar/Feedback.js
+++ client/components/navbar/Feedback.js
@@ -31,7 +31,7 @@ export default function Feedback() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-black bg-opacity-80 backdrop-blur-sm"/>
+            <div className="fixed inset-0 bg-black bg-opacity-80"/>
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
