734f4d5bdeb5982d00ae28b4d191fa7bb19ec1a6
Ramez Joseph <ramezjoseph8@gmail.com>
2023-11-24 02:03:55 -0800

    width fix for mobile and desktop

--- client/components/b/NewBoard.js
+++ client/components/b/NewBoard.js
@@ -2,6 +2,7 @@ export default function NewBoard(props) {
     return (
         <>
         <div className='h-100 2xl:w-3/6 w-full h-full min-h-screen '>
+            <div className="w-5/6">
             <div className='gap-4 flex float-right'>
             <button className='px-4 py-2 bg-indigo-500 rounded-md float-right flex'>
                 Create a new post
@@ -12,8 +13,9 @@ export default function NewBoard(props) {
                 Filter
             </button>
             </div>
+            </div>
             <br /><br /><br />
-            <div>
+            <div className='w-5/6'>
                 {props.children}
             </div>
         </div>
