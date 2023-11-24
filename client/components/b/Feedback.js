07052113850e45ffd59b6a550f1ea5b656fd53d3
Ramez Joseph <ramezjoseph8@gmail.com>
2023-11-24 04:21:58 -0800

    last ui update, need to study

--- client/components/b/Feedback.js
+++ client/components/b/Feedback.js
@@ -5,13 +5,13 @@ export default function Feedback(props) {
         <>
         <div className="w-full flex justify-between bg-gray-950 h-24 rounded-2xl items-center">
         <div className="m-8">
-        <h1>Left</h1>
+        <img class="w-10 h-10 rounded-full shadow-xl" src="https://pbs.twimg.com/profile_images/1684753206264430592/iumsH271_400x400.jpg" alt="Rounded avatar"></img>
         </div>
         <div className="m-8">
-        <div className="w-14 h-12 bg-gray-900 rounded-xl items-center flex justify-center">
-        <h1 className="m-1 font-bold">0</h1>
+        <button className="w-14 h-12 shadow-xl bg-gray-900 rounded-xl items-center flex justify-center">
+        <h1 className="m-1 font-medium">0</h1>
         <ChevronUpIcon className="w-7 h-7 text-white"/>
-        </div>
+        </button>
         </div>
         </div>
         </>
--- client/components/b/NewBoard.js
+++ client/components/b/NewBoard.js
@@ -4,13 +4,13 @@ import { HandThumbUpIcon} from "@heroicons/react/24/solid"
 export default function NewBoard(props) {
     return (
         <>
-        <div className='h-100 2xl:w-3/6 w-full h-full min-h-screen '>
+        <div className='h-100 2xl:w-3/6 lg:w-3/5 w-full h-full min-h-screen'>
             <div className="w-5/6 flex justify-between">
             <div className='gap-4 flex float-left'>
             <Dropdown />
             </div>
             <div className='gap-4 flex'>
-            <button className='px-4 py-2.5 bg-indigo-500 rounded-xl flex font-medium'>
+            <button className='px-4 py-2.5 bg-blue-600 rounded-xl flex font-medium'>
                 Submit Feedback
                 <HandThumbUpIcon className='mt-[0.15rem] ml-2 h-5 w-5 text-white hover:text-gray-100'/>
                 {/* <PlusCircleIcon className='mt-[0.15rem] ml-2 h-5 w-5 text-white hover:text-gray-100'/> */}
