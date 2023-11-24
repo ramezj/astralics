1130777e2ef986a13f20d6ef4aaf319cde0a6205
Ramez Joseph <ramezjoseph8@gmail.com>
2023-11-24 03:49:14 -0800

    Dropdown component from headlessui

--- client/components/b/NewBoard.js
+++ client/components/b/NewBoard.js
@@ -1,15 +1,15 @@
+import Dropdown from "./Dropdown"
+
 export default function NewBoard(props) {
     return (
         <>
         <div className='h-100 2xl:w-3/6 w-full h-full min-h-screen '>
             <div className="w-5/6 flex justify-between">
             <div className='gap-4 flex float-left'>
-            <button className='px-4 py-2 bg-zinc-950 rounded-xl flex'>
-                Filter
-            </button>
+            <Dropdown />
             </div>
             <div className='gap-4 flex'>
-            <button className='px-4 py-2 bg-indigo-500 rounded-xl flex'>
+            <button className='px-4 py-2 bg-indigo-500 rounded-xl flex font-medium'>
                 Create a new post
             </button>
             </div>
