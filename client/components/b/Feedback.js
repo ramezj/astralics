f64539102f9e62bd4ad78b6364f3118f3eb997b5
Ramez Joseph <ramezjoseph8@gmail.com>
2023-11-26 03:58:50 -0800

    text-left debug test

--- client/components/b/Feedback.js
+++ client/components/b/Feedback.js
@@ -4,9 +4,9 @@ export default function Feedback(props) {
     return (
         <>
         <div className="w-full flex bg-zinc-950 rounded-2xl items-center">
-        <div className="m-8 flex flex-col items-start">
+        <div className="m-8 flex flex-col items-start text-left">
         <p className='text-lg font-medium text-left'>{props.title}</p>
-        <p className='text-xs  text-left text-gray-200'>{props.description}</p>
+        <p className='text-xs text-left text-gray-200'>{props.description}</p>
         {/* <img class="2xl:w-10 2xl:h-10 md:w-9 md:h-9 w-7 h-7 rounded-full shadow-2xl" src="https://pbs.twimg.com/profile_images/1684753206264430592/iumsH271_400x400.jpg" alt="Rounded avatar"></img> */}
         </div>
         <div className="m-8 ml-auto">
