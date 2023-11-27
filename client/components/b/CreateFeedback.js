cc100985a0db13f60bda570e91b937ffe103d505
Ramez Joseph <ramezjoseph8@gmail.com>
2023-11-27 07:50:52 -0800

    changed button color [test]

--- client/components/b/CreateFeedback.js
+++ client/components/b/CreateFeedback.js
@@ -77,7 +77,7 @@ export default function CreateFeedback(props) {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl backgroundColor p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-blue-500 p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-xl font-medium leading-6 text-white"
