219776a455a7e5a178b94bbb9299ea6fabde8037
Ramez Joseph <ramezjoseph8@gmail.com>
2023-11-28 13:08:47 -0800

    border border-white/10

--- client/components/b/Feedback.js
+++ client/components/b/Feedback.js
@@ -3,7 +3,7 @@ import { ChevronUpIcon } from "@heroicons/react/24/outline"
 export default function Feedback(props) {
     return (
         <>
-        <div className="w-full flex bg-zinc-950 rounded-2xl items-center cursor-pointer duration-300">
+        <div className=" w-full flex bg-zinc-950 rounded-2xl items-center cursor-pointer duration-300">
         <div className="m-8 flex flex-col items-start text-left">
         <p className='text-lg font-bold text-left'>{props.title}</p>
         <p className='text-xs text-left text-gray-200'>{props.description}</p>
@@ -11,7 +11,7 @@ export default function Feedback(props) {
             props.type == "🐛 Bug Report"
             ? 
             <>
-            <span class="mt-2 bg-indigo-900 text-indigo-200 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:bg-indigo-900 dark:text-indigo-300">{props.type}</span>
+            <span class="border border-white/10 mt-2 -mb-1 bg-indigo-900 text-indigo-200 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:bg-indigo-900 dark:text-indigo-300">{props.type}</span>
             </>
             : <></>
         }
@@ -19,7 +19,7 @@ export default function Feedback(props) {
             props.type == "💡 Feature Request"
             ? 
             <>
-            <span class="mt-2 bg-yellow-900 text-yellow-200 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:bg-indigo-900 dark:text-indigo-300">{props.type}</span>
+            <span class="border border-white/10 mt-2 -mb-1 bg-yellow-900 text-yellow-200 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:bg-indigo-900 dark:text-indigo-300">{props.type}</span>
             </>
             : <></>
         }
@@ -27,14 +27,14 @@ export default function Feedback(props) {
             props.type == "📝 Feedback"
             ? 
             <>
-            <span class="mt-2 bg-green-900 text-green-200 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:bg-indigo-900 dark:text-indigo-300">{props.type}</span>
+            <span class="border border-white/10 mt-2 -mb-1 bg-green-900 text-green-200 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md dark:bg-indigo-900 dark:text-indigo-300">{props.type}</span>
             </>
             : <></>
         }
         {/* <img class="2xl:w-10 2xl:h-10 md:w-9 md:h-9 w-7 h-7 rounded-full shadow-2xl" src="https://pbs.twimg.com/profile_images/1684753206264430592/iumsH271_400x400.jpg" alt="Rounded avatar"></img> */}
         </div>
         <div className="m-8 ml-auto">
-        <button className="w-[4.5rem] h-12 shadow-xl bg-zinc-900 rounded-xl items-center flex justify-center">
+        <button className="border border-white/10 w-[4.5rem] h-12 shadow-xl bg-zinc-900 rounded-xl items-center flex justify-center">
         <h1 className="m-1 font-medium">{props.upvotes}</h1>
         <ChevronUpIcon className="w-7 h-7 text-white"/>
         </button>
