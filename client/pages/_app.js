5f05456f75acefd0d7bb8a3150b22803e4a74b6d
Ramez Joseph <ramezjoseph8@gmail.com>
2023-12-03 19:44:00 +0200

    new font, next conf changed

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 import ErrorBoundary from '@/components/ErrorBoundary'
+import { GeistSans } from "geist/font/sans";
+
+
 
 export default function App({
   Component,
@@ -9,7 +12,9 @@ export default function App({
   return (
     <ErrorBoundary>
     <SessionProvider session={session}>
+    <main className={GeistSans.className}>
       <Component className='' {...pageProps} />
+      </main>
     </SessionProvider>
     </ErrorBoundary>
   )
