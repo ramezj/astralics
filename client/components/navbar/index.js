e82baaca5f74dcfeda66d0393fff6aae2270ed97
Ramez Joseph <ramezjoseph8@gmail.com>
2023-12-05 23:24:01 +0200

    fixed margin in mobile responsive navbar

--- client/components/navbar/index.js
+++ client/components/navbar/index.js
@@ -75,7 +75,7 @@ export default function Navbar(props) {
               {/* <Settings /> */}
               <Link href='/app' className='rounded-xl px-7 py-2 bg-white hover:bg-gray-200 text-black font-bold flex gap-2 content-center justify-center items-center duration-200'>
                 {/* <Squares2X2Icon className='text-white' width={22} height={22}/> */}
-                Dashboard 
+                dashboard 
                 </Link></>
               : <> 
                 <button className='px-8 py-2 bg-white text-black hover:bg-gray-200 rounded-xl font-bold flex justify-center items-center gap-2 duration-200' onClick={openModal}>
@@ -168,13 +168,13 @@ export default function Navbar(props) {
                {
               props.session 
               ? <> 
-              <Link href='/settings' className='text-black px-8 py-2 bg-white hover:bg-gray-200 duration-200 rounded-xl font-bold flex justify-center gap-2'>
+                {/* <Link href='/settings' className='text-black px-8 py-2 bg-white hover:bg-gray-200 duration-200 rounded-xl font-bold flex justify-center gap-2'>
                 Settings 
-                </Link>
-              <Link href='/app' className='text-black mt-3 px-8 py-2 bg-white hover:bg-gray-200  duration-300 rounded-xl font-bold flex justify-center gap-2'>
-                Dashboard
+                </Link> */}
+              <Link href='/app' className='text-black -mt-2 px-8 py-2 bg-white hover:bg-gray-200  duration-300 rounded-xl font-bold flex justify-center gap-2'>
+                dashboard
                 </Link></>
-              : <><Link onClick={signUserIn} href='/' className='text-black px-8 py-2 bg-white hover:bg-gray-200 rounded-xl font-bold flex justify-center gap-2 duration-200'>
+              : <><Link onClick={signUserIn} href='/' className='text-black -mt-2 px-8 py-2 bg-white hover:bg-gray-200 rounded-xl font-bold flex justify-center gap-2 duration-200'>
               try lunar 
               </Link></>
               }
