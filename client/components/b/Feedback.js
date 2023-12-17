886f126cab12cd28ae8f75ce48a0a2196961e59e
Ramez Joseph <ramezjoseph8@gmail.com>
2023-12-18 00:30:00 +0200

    changed the not upvoted button, looks 10x better now

--- client/components/b/Feedback.js
+++ client/components/b/Feedback.js
@@ -97,13 +97,13 @@ export default function Feedback(props) {
             }
         })}
         className={`group hover:bg-blue-700 duration-300 w-[4.5rem] h-12 shadow-sm rounded-xl items-center flex justify-center 
-        ${test ? 'bg-blue-700 border-none hover:bg-blue-700' : 'bg-white'}`}>
+        ${test ? 'bg-blue-700 border-none hover:bg-blue-700' : 'bg-transparent border border-white/10'}`}>
         <h1 
-        className={`m-1 font-bold group-hover:text-white ${test ? 'text-white' : 'text-blue-700'}`}>
+        className={`m-1 font-bold group-hover:text-white ${test ? 'text-white' : 'text-white'}`}>
         {upvotes}</h1>
         <ChevronUpIcon 
         strokeWidth={2.5}
-        className={`w-6 h-6 group-hover:text-white strokeWidth={2} ${test ? 'text-white' : 'text-blue-700'}`}/>
+        className={`w-6 h-6 group-hover:text-white strokeWidth={2} ${test ? 'text-white' : 'text-white'}`}/>
         </button>
         </div>
         </div>
