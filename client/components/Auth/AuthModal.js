6e9a5f43fcd6cf86d14e1d4051b1b83baeedaa9b
Ramez Joseph <ramezjoseph8@gmail.com>
2024-01-01 21:24:29 +0200

    Authentication Modal fixed

--- client/components/Auth/AuthModal.js
+++ client/components/Auth/AuthModal.js
@@ -6,15 +6,15 @@ export default function AuthModal() {
     }
     return (
         <>
-        <div className="bg-zinc-950 border border-white/10 rounded-xl">
+        <div className="bg-white border border-white/10 rounded-lg">
         <center>
             <br/>
-                  <h1 className="-mt-1 font-bold text-lg">Sign In</h1>
-                  <button className='mt-4 outline-none px-8 py-2 bg-white text-black rounded-xl font-bold flex justify-center items-center gap-2 duration-300' onClick={signUserIn}>
+                  <h1 className="-mt-1 font-bold text-lg text-black">Authentication</h1>
+                  <button className='mt-4 outline-none w-4/5 py-2 bg-zinc-950 text-white rounded-lg font-medium flex justify-center items-center gap-2 duration-300' onClick={signUserIn}>
                   Sign in with Google</button> 
-                  <button className='mt-2 outline-none px-8 py-2 bg-white text-black rounded-xl font-bold flex justify-center items-center gap-2 duration-300' onClick={signIn}>
+                  <button className='mt-2 outline-none w-4/5 py-2 bg-zinc-950 text-white rounded-lg font-medium flex justify-center items-center gap-2 duration-300' onClick={signIn}>
                   Sign in with Github</button> 
-                  <p className="text-xs mt-4 -mb-4">powered by lunarfeed</p>
+                  <p className="text-xs mt-4 -mb-4 text-black font-medium">powered by lunarfeed</p>
                   <br />
         </center>
         </div>
