86bdb0ddb0163fb45fb35eb60b64b2262108edac
Ramez Joseph <ramezjoseph8@gmail.com>
2024-01-02 13:20:56 +0200

    added 'Enterprise' plan :)

--- client/pages/pricing/index.js
+++ client/pages/pricing/index.js
@@ -29,6 +29,7 @@ export default function Pricing(props) {
                 <PricingCard price='$0' title='Starter' button='Get Started' background={'bg-white'} shadow={'shadow-lg'}>
                 <Info><Svg /> 1 Board</Info>
                 <Info><Svg /> 50 Monthly Feedbacks</Info>
+                <Info><Svg /> Authentication Check</Info>
                 <Info><Xsvg /> Custom Domain </Info>
                 <br />
                 {
@@ -39,7 +40,31 @@ export default function Pricing(props) {
                 </PricingCard >
                 <PricingCard price='$8' title='Developer' button='Get Started' background={'bg-white'} shadow={'shadow-lg'}>
                 <Info><Svg /> Up to 3 Boards</Info>
+                <Info><Svg /> Up to 200 Monthly Feedbacks</Info>
+                <Info><Svg /> Authentication Check</Info>
+                <Info><Svg /> Custom Domain</Info>
+                <br />
+                {
+                session 
+                ? <><button onClick={(() => {
+                    Paddle.Checkout.open({
+                      product:63325,
+                      success:'/success',
+                      passthrough: session.user.id
+                    })
+                })} 
+                className='btn w-full text-white normal-case bg-black hover:bg-black outline-none border-none font-medium rounded-lg'>
+                    <CreditCardIcon className='text-white' width={22} height={22} />
+                 Subscribe Now</button></>
+                : <><button onClick={signUserIn} className='btn w-full text-white normal-case bg-black hover:bg-black outline-none border-none font-medium rounded-lg'>
+                    <CreditCardIcon className='text-white' width={22} height={22} />
+                    Get Started</button></>
+                }
+                </PricingCard >
+                <PricingCard price='$100' title='Enterprise' button='Get Started' background={'bg-white'} shadow={'shadow-lg'}>
+                <Info><Svg /> Unlimited Boards</Info>
                 <Info><Svg /> Unlimited Monthly Feedbacks</Info>
+                <Info><Svg /> Authentication Check</Info>
                 <Info><Svg /> Custom Domain</Info>
                 <br />
                 {
