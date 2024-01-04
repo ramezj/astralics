7a2177d798cfada2587bf2c784226883e5ba1aff
Ramez Joseph <ramezjoseph8@gmail.com>
2024-01-04 16:16:12 +0200

    messing around with Avatar component, taking break for finals

--- client/components/navbar/Avatar.js
+++ client/components/navbar/Avatar.js
@@ -22,17 +22,17 @@ export default function Avatar(props) {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="border border-white/10 absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-lg bg-zinc-950 shadow-lg ring-1 ring-black/5 focus:outline-none">
+          <Menu.Items className="border border-white/10 absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-lg bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
             <div className="px-1 py-1 ">
               <Menu.Item>
                 {({ active }) => (
                   <Link href='/app'>
                   <button
                     className={`${
-                      active ? 'bg-zinc-800 text-gray-100' : 'text-white'
+                      active ? 'bg-gray-200 text-black' : 'text-black'
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm font-medium`}
                   >
-                     My Boards
+                     Feedback Boards
                   </button>
                   </Link>
                 )}
@@ -42,10 +42,10 @@ export default function Avatar(props) {
                   <Link href='/settings'>
                   <button
                     className={`${
-                      active ? 'bg-zinc-800 text-gray-100' : 'text-white'
+                      active ? 'bg-gray-200 text-black' : 'text-black'
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm font-medium`}
                   >
-                    Account Settings
+                    User Settings
                   </button>
                   </Link>
                 )}
@@ -54,7 +54,7 @@ export default function Avatar(props) {
                 {({ active }) => (
                   <button
                     className={`${
-                      active ? 'bg-zinc-800 text-gray-100' : 'text-white'
+                      active ? 'bg-gray-200 text-black' : 'text-black'
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm font-medium`}
                   >
                     Sign Out
