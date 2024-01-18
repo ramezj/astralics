5d0085b1cf2bce5bab9e6161aeaab8ef1d70d94b
Ramez Joseph <ramezjoseph8@gmail.com>
2024-01-19 01:30:23 +0200

    fixed some navbar rounded issues in appnav

--- client/components/app/Billing.js
+++ client/components/app/Billing.js
@@ -5,7 +5,7 @@ import Link from "next/link"
 export default function Billing(props) {
     return (
         <>
-         <Link href='/billing' className='px-5 py-2 bg-white text-black hover:bg-gray-200 rounded-md flex justify-center items-center gap-2 duration-200'>
+         <Link href='/billing' className='px-5 py-2 bg-white text-black hover:bg-gray-200 rounded-lg flex justify-center items-center gap-2 duration-200'>
             Billing
         </Link>
         </>
--- client/components/app/Create.js
+++ client/components/app/Create.js
@@ -47,7 +47,7 @@ export default function Create() {
     }
     return (
         <>
-        <button className='px-5 py-2 bg-white text-black hover:bg-gray-200 rounded-md flex justify-center items-center gap-2 duration-200' onClick={openModal}>
+        <button className='px-5 py-2 bg-white text-black hover:bg-gray-200 rounded-lg flex justify-center items-center gap-2 duration-200' onClick={openModal}>
               Create Board
         </button>
 <Transition appear show={isOpen} as={Fragment}>
