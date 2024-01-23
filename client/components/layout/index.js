3f2a7bf78c5055ed5cbfe67694b6fbe23df34214
Ramez Joseph <ramezjoseph8@gmail.com>
2024-01-23 02:30:11 +0200

    some adjustments

--- client/components/layout/index.js
+++ client/components/layout/index.js
@@ -4,6 +4,7 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Layout(props) {
     const { data: session } = useSession()
     return (
+        // background3
         <div className='background3 h-full min-h-screen bg-bottom bg-no-repeat w-full'>
             {/* bg-[#05050a] */}
         <Navbar session={session}/>
