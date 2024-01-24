14d04426cf721316d1eb764d7adf47531f54ef74
daddylunix <ramezjoseph8@gmail.com>
2024-01-24 15:47:33 +0200

    Fixed height difference in Toggle button

--- client/components/navbar/Toggle.js
+++ client/components/navbar/Toggle.js
@@ -17,11 +17,23 @@ export function Toggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button  className='py-2 border border-black/20 dark:border-white/10 hover:border-black/0 dark:hover:border-white/0 duration-200 rounded-lg bg-white dark:bg-zinc-900 text-black dark:text-white hover:bg-gray-200 dark:hover:bg-zinc-800'>
+      <button className='w-[3.5rem] py-2 
+        outline-none
+        bg-white
+        dark:bg-zinc-900
+        dark:hover:bg-zinc-800
+        dark:text-white
+        border
+        border-black/20
+        hover:border-black/0
+        dark:border-white/10
+        dark:hover:border-white/0
+        text-black hover:bg-gray-200 rounded-lg flex justify-center items-center gap-2
+        duration-200'>
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonStarIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
-        </Button>
+        </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className='dark:bg-zinc-900 border dark:border-white/10'>
         <DropdownMenuItem onClick={() => setTheme("light")}>
