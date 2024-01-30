c42c11aaab4c586d12d104dbc713e3cdcee749cd
Ramez Joseph <ramezjoseph8@gmail.com>
2024-01-30 16:20:13 +0200

    redeploy of previous deployment

--- client/components/app/createBoard.js
+++ client/components/app/createBoard.js
@@ -42,7 +42,7 @@ export default function CreateBoard(props) {
             setText("Created Successfully");
             setResponse(resp);
             console.log(resp);
-            // router.push(`/board/${resp.response.id}`)
+            router.push(`/b/${resp.response.id}`)
         } else if (resp.ok == false) {
             setLoading(false);
             setText(resp.response);
@@ -89,26 +89,6 @@ export default function CreateBoard(props) {
       </CardFooter>
       </form>
     </Card>
-        {/* <div className="card w-[22.5rem] bg-white dark:bg-zinc-950 border border-black/10 dark:border-white/10 duration-300 rounded-lg">
-        <div className="card-body">
-        <h2 className="card-title text-xl text-black dark:text-white font-bold justify-center -mt-2 mb-2 drop-shadow">Board Information</h2>
-        <form onSubmit={createProject}>
-        <Input required autoFocus={false} type="text" placeholder="Board name" onChange={(e) => {setName(e.target.value)}} className="input-md w-full mt-2 focus:outline-none bg-gray-200 dark:bg-zinc-900 text-black dark:text-white font-bold text-sm rounded-md" />
-        <Input required autoFocus={false} type="url" placeholder="Board Website" onChange={(e) => {setUrl(e.target.value)}} className="input-md w-full mt-2 focus:outline-none bg-gray-200 dark:bg-zinc-900 text-black dark:text-white font-bold text-sm mt-4 rounded-md" />
-        <Input required autoFocus={false} type="text" placeholder="Board Handle" onChange={(e) => {setHandle(e.target.value)}} className="input-md w-full mt-2 focus:outline-none bg-gray-200 dark:bg-zinc-900 text-black dark:text-white font-bold text-sm mt-4 rounded-md" />
-        <center>
-        <button 
-        className="-mb-1 mt-6 w-full py-2 rounded-lg text-base text-white normal-case bg-blue-700 hover:bg-blue-800 duration-300 outline-none border-none font-medium" type='submit'>
-        { 
-        loading 
-        ? <><><span className="loading loading-spinner loading-xs"></span></></>
-        : <>{text}</>
-        }
-        </button>
-        </center>
-        </form>
-        </div>
-        </div> */}
         </>
     )
 }
