a75d048f26ddc1159f48669dadd88cec9e2a8b25
Ramez Joseph <ramezjoseph8@gmail.com>
2024-01-31 17:01:05 +0200

    Navbar in all layouts [ temp ]

--- client/components/app/AppLayout.js
+++ client/components/app/AppLayout.js
@@ -1,5 +1,6 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 import AppNavbar from "./AppNavbar"
+import Navbar from "../navbar"
 import Header from "../NewNav/Header"
 import Sidebar from "./Sidebar"
 
@@ -7,7 +8,7 @@ export default function AppLayout(props) {
     const { data: session } = useSession()
     return (
         <div className='bg-white dark:bg-zinc-950 h-full min-h-screen bg-bottom bg-no-repeat w-full'>
-        <Header session={session} />
+        <Navbar session={session}/>
         {/* <Sidebar /> */}
         {props.children}
         </div>
--- client/components/b/Layout.js
+++ client/components/b/Layout.js
@@ -1,4 +1,4 @@
-import {Navbar} from "../navbar";
+import Navbar from "../navbar";
 import { useSession, signIn, signOut } from "next-auth/react"
 import BoardNav from "./BoardNav";
 import Header from "../NewNav/Header";
@@ -7,7 +7,7 @@ export default function BoardLayout(props) {
     const { data: session } = useSession()
     return (
         <div className='bg-white dark:bg-zinc-950 min-h-screen bg-bottom bg-no-repeat w-full bg-cover'>
-        <Header session={session}/>
+        <Navbar session={session}/>
         {props.children}
         </div>
     )
