01bf3acfbd5125fc15c21dba4c429da9a7993b19
Ramez Joseph <ramezjoseph8@gmail.com>
2024-01-31 14:14:12 +0200

    navbar sticky now

--- client/components/navbar/index.js
+++ client/components/navbar/index.js
@@ -27,7 +27,7 @@ export default function Navbar(props) {
     }
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
-    <header className="bg-transparent">
+    <header className="bg-transparent sticky top-0 z-10">
       <nav className="h-14 mx-auto max-w-full flex items-center justify-between p-6 lg:px-3 border-b border-border/70 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link href="/" className="p-1.5 outline-none font-medium italic text-black dark:text-white">
