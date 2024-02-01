eb6c6b4b4be214b810072b8463fbaf4f0fd78fe0
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-01 13:52:15 +0200

    TryForFree button colors changed

--- client/components/navbar/TryForFree.js
+++ client/components/navbar/TryForFree.js
@@ -21,16 +21,16 @@ export const TryForFree = (props) => {
         <>
         <Button className='w-[8rem]
         text-sm
-        bg-gray-200
-        dark:bg-zinc-900
-        dark:hover:bg-zinc-800
-        dark:text-white
+        bg-zinc-950
+        dark:bg-white
+        dark:hover:bg-gray-300
+        dark:text-black
         border
         border-none
         hover:border-black/0
         dark:border-white/10
         dark:hover:border-white/0
-        text-black hover:bg-gray-300 rounded-lg items-center
+        text-white hover:bg-zinc-800 rounded-lg items-center
         duration-200'
         onClick={openModal}>
         {props.title}
