15cf56b60f12c09fb1d5676ebb340b043f9d0090
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-14 16:23:15 +0200

    new AuthModal

--- client/components/Auth/AuthModal.js
+++ client/components/Auth/AuthModal.js
@@ -1,4 +1,6 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import { GoogleSVG } from "./GoogleSVG";
+import { GithubSVG } from "./GithubSVG";
 
 export default function AuthModal() {
     const signUserIn = () => {
@@ -9,10 +11,10 @@ export default function AuthModal() {
         <div className="bg-white border border-white/10 rounded-lg">
         <center>
             <br/>
-                  <h1 className="-mt-1 font-bold text-lg text-black">Authentication</h1>
+                  <h1 className="-mt-1 font-bold text-xl text-black">Sign in to your account</h1>
                   <button className='mt-4 outline-none w-4/5 py-2 bg-zinc-950 text-white rounded-lg font-medium flex justify-center items-center gap-2 duration-300' onClick={signUserIn}>
-                  Continue with Google</button> 
-                  <p className="text-xs mt-4 -mb-4 text-black font-medium">powered by lunarfeed</p>
+                  <GoogleSVG className=''/>
+                  Continue with Google</button>
                   <br />
         </center>
         </div>
