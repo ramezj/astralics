1549f1ed897e288018a736062ce9fa88bcb3a9ad
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-14 14:03:39 +0200

    started working on AuthModal

--- client/components/Auth/AuthModal.js
+++ client/components/Auth/AuthModal.js
@@ -11,9 +11,7 @@ export default function AuthModal() {
             <br/>
                   <h1 className="-mt-1 font-bold text-lg text-black">Authentication</h1>
                   <button className='mt-4 outline-none w-4/5 py-2 bg-zinc-950 text-white rounded-lg font-medium flex justify-center items-center gap-2 duration-300' onClick={signUserIn}>
-                  Sign in with Google</button> 
-                  <button className='mt-2 outline-none w-4/5 py-2 bg-zinc-950 text-white rounded-lg font-medium flex justify-center items-center gap-2 duration-300' onClick={signIn}>
-                  Sign in with Github</button> 
+                  Continue with Google</button> 
                   <p className="text-xs mt-4 -mb-4 text-black font-medium">powered by lunarfeed</p>
                   <br />
         </center>
