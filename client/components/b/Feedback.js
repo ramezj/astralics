e676b1843660acaead63b90fdb5802d62653a215
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-14 21:15:50 +0200

    some ui updates

--- client/components/b/Feedback.js
+++ client/components/b/Feedback.js
@@ -197,6 +197,7 @@ export default function Feedback(props) {
                   </CardHeader>
                   <CardContent>
                     <h2>{props.description}</h2>
+                    
                   </CardContent>
                   <CardFooter className="flex justify-between gap-x-2">
                     <Button type='button' onClick={closeM} variant="outline" className='w-full bg-gray-200 text-black dark:bg-zinc-950 dark:text-white dark:border-white/10 dark:hover:!bg-zinc-900 hover:!bg-gray-300 duration-200 border-black/10'>Cancel</Button>
