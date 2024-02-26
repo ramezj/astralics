5a80b7551d54b58e6fb407d7cfe0647c6a547e1c
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-26 14:28:33 +0200

    more tests of the webhook

--- client/pages/api/hooks/index.js
+++ client/pages/api/hooks/index.js
@@ -10,28 +10,25 @@ export default async function handler(req, res) {
         })
     }
     try {
-        // Catch the event type
-        const clonedReq = req.clone();
-        const eventType = req.headers.get("X-Event-Name");
-        const body = await req.json();
-    
-        // Check signature
+        const rawBody = await req.text();
         const secret = "Q2HDAQ89BHDA728BDAIUBDA8727DB";
-        const hmac = crypto.createHmac("sha256", secret);
-        const digest = Buffer.from(
-          hmac.update(await clonedReq.text()).digest("hex"),
-          "utf8"
-        );
-        const signature = Buffer.from(req.headers.get("X-Signature") || "", "utf8");
-    
+        const hmac = crypto.createHmac('sha256', secret);
+        const digest = Buffer.from(hmac.update(rawBody).digest('hex'), 'utf8');
+        const signature = Buffer.from(req.headers.get('X-Signature') || '', 'utf8');
         if (!crypto.timingSafeEqual(digest, signature)) {
-          throw new Error("Invalid signature.");
+          throw new Error('Invalid signature.');
         }
-        console.log(body);
-        return Response.json({ message: "Webhook received" });
+        const data = JSON.parse(rawBody)
+        const eventName = data['meta']['event_name']
+        const obj = data['data']['attributes']
+        const objId = data['data']['id']
+        console.log(eventName)
       } catch (err) {
         console.error(err);
-        console.error(err);
-        return Response.json({ message: "Server error" }, { status: 500 });
+        return res.status(400).json({
+            ok:false,
+            response: 'Something went completely wrong',
+            error:err
+        })
       }
  }
