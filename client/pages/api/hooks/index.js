5e6fcff5ecb2cf240c0149a515cc67dbe21d338f
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-26 16:24:09 +0200

    text test

--- client/pages/api/hooks/index.js
+++ client/pages/api/hooks/index.js
@@ -1,7 +1,7 @@
 // for paddle webhooks
 import { prisma } from "@/lib/db"
 import crypto from "crypto"
-import { buffer } from "micro";
+import { buffer, text } from "micro";
 
 export default async function handler(req, res) {
     if(req.method !== 'POST') {
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     }
     const secret = "Q2HDAQ89BHDA728BDAIUBDA8727DB";
     const hmac = crypto.createHmac('sha256', secret);
-	const digest = Buffer.from(hmac.update(req.body).digest('hex'), 'utf8');
+	const digest = Buffer.from(hmac.update(text(req.body)).digest('hex'), 'utf8');
 	const signature = Buffer.from(req.headers['x-signature'] || '', 'utf8');
     if(!crypto.timingSafeEqual(digest, signature)) {
         return res.status(400).json({
