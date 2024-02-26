e09629467b4f43a1f2e28ab992af8381d8588841
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-26 16:51:00 +0200

    console logging some stuff

--- client/pages/api/hooks/index.js
+++ client/pages/api/hooks/index.js
@@ -15,12 +15,16 @@ export default async function handler(req, res) {
     const rawBody = (await buffer(req)).toString('utf-8')
 	const digest = Buffer.from(hmac.update(rawBody).digest('hex'), 'utf8');
 	const signature = Buffer.from(req.headers['x-signature'] || '', 'utf8');
+    console.log("Lemon-Squeezy Signature :", signature);
+    console.log("Digest :", digest);
     if(!crypto.timingSafeEqual(digest, signature)) {
+        console.log('Invalid Signature..')
         return res.status(400).json({
             ok:false,
             response:'Invalid signature'
         })
     }
+    console.log("Signature verified");
     return res.status(200).json({
         ok:true,
         response:'Webhook received successfully!'
