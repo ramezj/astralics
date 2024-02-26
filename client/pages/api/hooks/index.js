b818cc19b0f5f518632dd0fc971e23f3a97d1b42
Ramez Joseph <ramezjoseph8@gmail.com>
2024-02-26 16:19:00 +0200

    webhook

--- client/pages/api/hooks/index.js
+++ client/pages/api/hooks/index.js
@@ -10,27 +10,18 @@ export default async function handler(req, res) {
             response:'Invalid Method'
         })
     }
-    try {
-        const secret = "Q2HDAQ89BHDA728BDAIUBDA8727DB";
-        const rawBody = (await buffer(req)).toString('utf-8')
-        const hmac = crypto.createHmac('sha256', secret)
-        const digest = Buffer.from(hmac.update(rawBody).digest('hex'), 'utf8')
-        const signature = Buffer.from(req.headers['x-signature'], 'utf8')
-        if (!crypto.timingSafeEqual(digest, signature)) {
-          return res.status(400).json({
-            message: 'Invalid signature.',
-          })
-        }
-        console.log(req);
-        return res.status(200).json({
-            message:'Signatures are alike :)'
-        })
-      } catch (err) {
-        console.error(err);
+    const secret = "Q2HDAQ89BHDA728BDAIUBDA8727DB";
+    const hmac = crypto.createHmac('sha256', secret);
+	const digest = Buffer.from(hmac.update(req.body).digest('hex'), 'utf8');
+	const signature = Buffer.from(req.headers['x-signature'] || '', 'utf8');
+    if(!crypto.timingSafeEqual(digest, signature)) {
         return res.status(400).json({
             ok:false,
-            response: 'Something went completely wrong',
-            error:err
+            response:'Invalid signature'
         })
-      }
+    }
+    return res.status(200).json({
+        ok:true,
+        response:'Webhook received successfully!'
+    })
  }
