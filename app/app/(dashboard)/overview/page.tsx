ba6c59eb40b266a253f5ef351d0877f2dc25049c
daddylunix <ramezjoseph8@gmail.com>
2024-04-24 19:32:03 +0200

    JSON.STRINGIFY

--- app/app/(dashboard)/overview/page.tsx
+++ app/app/(dashboard)/overview/page.tsx
@@ -30,7 +30,7 @@ export default async function Page() {
                 </>
             )
         })}
-        {/* {JSON.stringify(pages)} */}
+        {JSON.stringify(pages)}
         </>
     )
 }
